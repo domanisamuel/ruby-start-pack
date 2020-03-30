806ebede6c883724233274ea3c53181f7e48407d
Sammie <sammie@Sammies-MBP.mshome.net>
2020-03-30 22:23:43 +0300

    variables

---
+++ functions.rb
@@ -0,0 +1,14 @@
+# functions
+# declare function
+def greeting
+    puts "hello ruby"
+end
+greeting() # call function
+
+# function with argument
+def greet(name)
+    puts "hello #{name}"
+end
+greet("Sam") # braces are optional
+
+
---
+++ variables.rb
@@ -0,0 +1,15 @@
+# variables
+
+name = "Samuel"
+score = 80
+
+puts(name)
+puts(score)
+
+# Assignment chaining
+x = y = z = 4
+puts x + y + z
+
+# Parallel assignment
+a,b = 5,6
+puts(a,b)
